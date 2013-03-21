0129c6fdd89469f6622fc487cd0b54fed4fd9b65
Şahin Dirim <sahanus48@gmail.com>
2013-03-21 02:51:27 +0200

    main eklendi
    main

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("deneme\n");
+    printf("kkkksklsa");
+   printf("kkkkskddlsa");
+   printf("kkkksklsa");
+   printf("kkkksklsa");
+   printf("kkkksklsa");
+   printf("kkkksklsa");
+
+
+
+
+    return 0;
+}
